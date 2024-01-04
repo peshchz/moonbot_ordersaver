9b6417e885123cb5ab3f9686c1d1d39859303cee
tema2002 <38882008+tema2002@users.noreply.github.com>
2024-01-04 12:38:11 +0700

    Update add_saver.ps1

--- PowerShell/add_saver.ps1
+++ PowerShell/add_saver.ps1
@@ -2,8 +2,6 @@
 $url2 = "http://yourdomain.ru/test-file/config.ini"
 $url3 = "http://yourdomain.ru/test-file/logging_config.ini"
 $url4 = "http://yourdomain.ru/test-file/new_schedule.ps1"
-$url5 = "http://yourdomain.ru/test-file/2updater_schedule.ps1"
-$url6 = "http://yourdomain.ru/test-file/updater.exe"
 
 $work_dir = "C:\_saver"
 $file_name = "ordersaver.exe"
@@ -11,8 +9,6 @@ $executable_file1 = $work_dir + "\" + $file_name
 $config_file2 = $work_dir + "\" + "config.ini"
 $config_file3 = $work_dir + "\" + "logging_config.ini"
 $config_file4 = $work_dir + "\" + "new_schedule.ps1"
-$config_file5 = $work_dir + "\" + "2updater_schedule.ps1"
-$executable_file2 = $work_dir + "\" + "updater.exe"
 
 
 if(!(Test-Path -path $work_dir))  
@@ -69,24 +65,3 @@ if (!(Test-Path -Path $config_file4 -PathType Leaf)) {
 
 }
 
-if (!(Test-Path -Path $config_file5 -PathType Leaf)) {
-# download config_file
-  try {
-         Invoke-WebRequest -Uri $url5 -OutFile $config_file5
-  }
-  catch {
-         throw $_.Exception.Message
-  }
-
-}
-
-if((Test-Path -path $work_dir)) {
-# download config_file
-  try {
-         Invoke-WebRequest -Uri $url6 -OutFile $executable_file2
-  }
-  catch {
-         throw $_.Exception.Message
-  }
-
-}
